ce46b0913496f2ec2b2b29453732d699040ab63f
Orcino Neto <orcinonetojp@gmail.com>
2024-05-10 13:46:54 -0300

    Adicionando Banco e as Proc de Cliente, Conta, Lancamento

---
+++ DB_ConstrutoraLMNC/Procedures/Cliente.sql
@@ -0,0 +1,250 @@
+CREATE OR ALTER PROCEDURE [dbo].[SP_InserirCliente] 
+	@Nome VARCHAR(500),
+	@Email VARCHAR(500),
+	@Senha VARCHAR(64),
+	@Cpf BIGINT,
+	@Telefone BIGINT,
+	@DataNascimento DATE
+	AS 
+	/*
+	Documentacao
+	Arquivo Fonte.....: Cliente.sql
+	Objetivo.............: Cria um cliente na tabela [dbo].[Cliente]
+	Autor.................: Orcino Neto
+	Data..................: 10/05/2024
+	Ex.....................: 
+								BEGIN TRAN
+									DBCC DROPCLEANBUFFERS;
+									DBCC FREEPROCCACHE;
+
+									DECLARE @RET INT, 
+									@Dat_init DATETIME = GETDATE()
+
+									SELECT	Id,
+												Nome,
+												Email,
+												Senha,
+												Cpf,
+												Telefone,
+												DataNascimento,
+												Ativo
+										FROM [dbo].[Cliente] 
+
+									EXEC @RET = [dbo].[SP_InserirCliente] 'Finado Betoneira','betuoneira@gmail.com','Beto1234',78965412324,83987741236,'2000-01-01'
+
+									SELECT @RET AS RETORNO,
+									DATEDIFF(millisecond, @Dat_init, GETDATE()) AS TempoExecucao
+
+									SELECT	Id,
+												Nome,
+												Email,
+												Senha,
+												Cpf,
+												Telefone,
+												DataNascimento,
+												Ativo
+										FROM [dbo].[Cliente] 
+								ROLLBACK TRAN
+
+		--	RETORNO   --
+		00.................: Erro ao criar conta
+		01.................: Sucesso																
+		*/
+		BEGIN
+				
+			INSERT INTO [dbo].[Cliente] (Nome,Email,Senha,Cpf,Telefone,DataNascimento,Ativo) VALUES   				
+														(@Nome,@Email,@Senha,@Cpf,@Telefone,@DataNascimento,1);
+
+			IF @@ROWCOUNT <> 0
+				RETURN 0
+			ELSE
+				RETURN 1
+				
+		END
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[SP_ExcluirCliente]
+		@Id_Cliente INT
+		AS
+        /*
+		Documentacao
+		Arquivo Fonte.....: Cliente.sql
+		Objetivo.............: Mudar para desativo um Cliente
+		Autor.................: Orcino Neto
+		Data..................: 10/05/2024
+		Ex.....................: 
+									BEGIN TRAN
+											DBCC DROPCLEANBUFFERS;
+											DBCC FREEPROCCACHE;
+
+												DECLARE @RET INT, 
+												@Dat_init DATETIME = GETDATE()
+
+												SELECT	Id,
+															Nome,
+															Email,
+															Senha,
+															Cpf,
+															Telefone,
+															DataNascimento,
+															Ativo
+													FROM [dbo].[Cliente] 
+
+												EXEC @RET = [SP_ExcluirCliente] 1
+
+												SELECT	Id,
+															Nome,
+															Email,
+															Senha,
+															Cpf,
+															Telefone,
+															DataNascimento,
+															Ativo
+													FROM [dbo].[Cliente] 
+
+												SELECT @RET AS RETORNO,
+													   DATEDIFF(millisecond, @Dat_init, GETDATE()) AS EXECUCAO 
+
+										ROLLBACK TRAN
+
+						--	RETORNO  --
+							00.................: Sucesso.
+							01.................:	Erro.
+	   */
+		BEGIN
+
+			UPDATE [dbo].[Cliente]  SET Ativo = 0
+				WHERE Id = @Id_Cliente			
+
+			IF @@ROWCOUNT <> 0
+				RETURN 0
+			ELSE
+				RETURN 1
+		END
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[SP_AtualizarSenhaCliente]
+		@Id_Cliente INT,
+		@Senha VARCHAR(64) 
+		
+		AS
+        /*
+		Documentacao
+		Arquivo Fonte.....: Cliente.sql
+		Objetivo.............: Alterar senha do cliente.
+		Autor.................: Orcino Neto
+		Data..................: 10/05/2024
+		Ex.....................: 
+									BEGIN TRAN
+											DBCC DROPCLEANBUFFERS;
+											DBCC FREEPROCCACHE;
+
+												DECLARE @RET INT, 
+												@Dat_init DATETIME = GETDATE()
+
+												SELECT	Id,
+															Nome,
+															Email,
+															Senha,
+															Cpf,
+															Telefone,
+															DataNascimento,
+															Ativo
+													FROM [dbo].[Cliente] 
+
+												EXEC @RET = [SP_AtualizarSenhaCliente] 1 , 'BetuMono360'
+
+												SELECT	Id,
+															Nome,
+															Email,
+															Senha,
+															Cpf,
+															Telefone,
+															DataNascimento,
+															Ativo
+													FROM [dbo].[Cliente] 
+
+												SELECT @RET AS RETORNO,
+													   DATEDIFF(millisecond, @Dat_init, GETDATE()) AS EXECUCAO 
+
+										ROLLBACK TRAN
+
+						--	RETORNO  --
+							00.................: Sucesso.
+							01.................:	Erro.
+	   */
+		BEGIN
+
+			UPDATE [dbo].[Cliente]  SET Senha = @Senha
+				WHERE Id = @Id_Cliente			
+
+			IF @@ROWCOUNT <> 0
+				RETURN 0
+			ELSE
+				RETURN 1
+
+		END
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[SP_AtualizarTelefoneCliente]
+		@Id_Cliente INT,
+		@Telefone BIGINT
+		
+		AS
+        /*
+		Documentacao
+		Arquivo Fonte.....: Cliente.sql
+		Objetivo.............: Alterar telefone do cliente.
+		Autor.................: Orcino Neto
+		Data..................: 10/05/2024
+		Ex.....................: 
+									BEGIN TRAN
+											DBCC DROPCLEANBUFFERS;
+											DBCC FREEPROCCACHE;
+
+												DECLARE @RET INT, 
+												@Dat_init DATETIME = GETDATE()
+
+												SELECT	Id,
+															Nome,
+															Email,
+															Senha,
+															Cpf,
+															Telefone,
+															DataNascimento,
+															Ativo
+													FROM [dbo].[Cliente] 
+
+												EXEC @RET = [SP_AtualizarTelefoneCliente] 1, 83988991664
+
+												SELECT	Id,
+															Nome,
+															Email,
+															Senha,
+															Cpf,
+															Telefone,
+															DataNascimento,
+															Ativo
+													FROM [dbo].[Cliente] 
+
+												SELECT @RET AS RETORNO,
+													   DATEDIFF(millisecond, @Dat_init, GETDATE()) AS EXECUCAO 
+
+										ROLLBACK TRAN
+
+						--	RETORNO  --
+							00.................: Sucesso.
+							01.................:	Erro.
+	   */
+		BEGIN
+
+			UPDATE [dbo].[Cliente]  SET Telefone = @Telefone
+				WHERE Id = @Id_Cliente
+
+			IF @@ROWCOUNT <> 0
+				RETURN 0
+			ELSE
+				RETURN 1
+
+		END
+GO
---
+++ DB_ConstrutoraLMNC/Procedures/Lancamentos.sql
@@ -0,0 +1,89 @@
+CREATE OR ALTER PROCEDURE [dbo].[SP_CriarLancamento]
+	@IdConta INT,
+	@IdTipo INT,
+	@IdTransferencia INT,
+	@IdDespesa INT,
+	@TipoOperacao CHAR(1),
+	@Valor DECIMAL(10,2),
+	@DataLancamento DATETIME,
+	@NomeHistorico VARCHAR(200)
+	AS
+	/*
+	Documentação
+	Arquivo Fonte..: Lancamentos.sql
+	Objetivo..........: Inserir Dados na Tabela Lancamentos, não permitir lancamentos futuros.
+							Se @DataLancamento for NULL recebe GetDate().
+	Autor..............: Orcino Neto
+	Data...............: 10/05/2024
+	Ex..................:	
+							BEGIN TRAN
+								DBCC DROPCLEANBUFFERS; 
+								DBCC FREEPROCCACHE;
+	
+								DECLARE	@Dat_init DATETIME = GETDATE(),
+												@RET INT
+								SELECT TOP 10 * FROM Lancamento
+	
+								EXEC @RET = [dbo].[SP_CriarLancamento]	1, 1, null, null,'D',200, null, 'Dinheiro de Pinga'
+								
+								SELECT TOP 10 * FROM Lancamento
+	
+								SELECT @RET AS RETORNO
+	
+								SELECT DATEDIFF(MILLISECOND, @Dat_init, GETDATE()) AS TempoExecucao
+							ROLLBACK TRAN
+
+		Lista de retornos:
+                1: Valor de Lançamento tem que ser maior que 0.
+                2: Não permitido lançamentos futuros.
+                3: Não permitido lançamentos de meses diferentes.
+                4: Erro ao inserir lançamento.
+	*/
+
+	BEGIN
+		DECLARE	@DataAtual DATETIME = GETDATE(),
+						@IdLancamento INT;
+
+		--Verificação se o lancamento é maior que 0.
+		IF @Valor < 0
+			BEGIN
+				 RETURN 1
+			END
+
+		--Verificando se o lançamento é com data futura.
+		IF @DataLancamento > DATEADD(MINUTE, DATEDIFF(MINUTE, @DataLancamento, @DataAtual), @DataLancamento)
+			BEGIN
+				 RETURN 2
+			END
+
+		--Verificando se o lançamento é do mes anterior.
+		IF DATEDIFF(MONTH,@DataLancamento, @DataAtual) <> 0
+			BEGIN
+				RETURN 3
+			END
+
+		--Inserindo Lançamento
+		INSERT INTO [dbo].[Lancamento]	(
+																IdConta,
+																IdTipo,
+																IdTransferencia,
+																IdDespesa,
+																TipoOperacao,
+																Valor,
+																DataLancamento,
+																NomeHistorico
+															)
+			VALUES (
+							@IdConta,
+							@IdTipo,
+							@IdTransferencia,
+							@IdDespesa,
+							@TipoOperacao,
+							@Valor,
+							ISNULL(@DataLancamento,@DataAtual),
+							@NomeHistorico
+						)
+
+			RETURN 0
+	END
+GO
