56411a0a49d72c7ba1fc1570433aa8451328279c
Adriel  Alexsander <alexander.sousa@smn.com.br>
2024-05-10 15:46:27 -0300

    Iniciando implementações Fluxo de caixa e TRansferencia

---
+++ Construtora/FUNCTIONS/FNC_CalcularSaldoAtualConta.sql
@@ -0,0 +1,37 @@
+CREATE OR ALTER FUNCTION [dbo].[FNC_CalcularSaldoAtualConta](
+														 @IdConta INT = NULL, 
+														 @ValorSldInicial DECIMAL(10,2) = NULL, 
+														 @ValorCredito DECIMAL(10,2) = NULL, 
+														 @ValorDebito DECIMAL(10,2) = NULL
+														)
+	 RETURNS  DECIMAL(10,2)
+	 AS 
+	 /*
+		Documentação
+		Arquivo Fonte.....: FNC_CalcularSaldoAtualConta.sql
+		Objetivo..........: Listar o saldo atual de todas as contas ou uma conta especifica na tabela Conta
+		Autor.............: Adriel Alexsander 
+ 		Data..............: 10/05/2024
+		Ex................: 
+							DBCC DROPCLEANBUFFERS;
+							DBCC FREEPROCCACHE;
+								
+							DECLARE @Dat_ini DATETIME = GETDATE()
+							SELECT	[dbo].[FNC_CalcularSaldoAtual](NULL,200,500,100) AS Resultado,
+									DATEDIFF(millisecond, @Dat_ini, GETDATE()) AS Tempo_Execucao	
+	*/
+	BEGIN
+		  --Verificar ID nulo
+		IF(@IdConta IS NOT NULL)
+			--Recuperar Valores 
+			BEGIN
+				SELECT @ValorSldInicial = ValorSaldoInicial,
+					   @ValorCredito = ValorCredito,
+					   @ValorDebito = ValorDebito
+					FROM [dbo].[Conta] WITH(NOLOCK)
+					WHERE Id = @IdConta
+			END
+			--Caso do Id nulo, vai usar os demais valores passados como parâmetros
+		RETURN @ValorSldInicial + @ValorCredito - @ValorDebito
+	END
+GO
