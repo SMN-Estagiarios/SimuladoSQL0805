1fcc6dee9e5d972eed9d162b782c6540573fa3e7
Orcino Neto <orcinonetojp@gmail.com>
2024-05-10 23:55:49 -0300

    Fazendo Alterações pendentes

---
+++ DB_ConstrutoraLMNC/Functions/FNC_CalcularValorParcela.sql
@@ -0,0 +1,39 @@
+﻿CREATE OR ALTER FUNCTION [dbo].[FNC_CalcularValorParcela](
+															@ValorVenda DECIMAL(10,2),
+															@Financiado BIT,
+															@TotalParcela SMALLINT,
+															@IdIndice TINYINT
+														 )
+	RETURNS DECIMAL(10,2)
+	AS
+	/*
+		Documenta��o
+		Arquivo Fonte.........:	FNC_CalcularValorParcela.sql
+		Objetivo..............:	Function para calcular o valor total a ser pago na parcela
+		Autor.................:	Grupo de Estagiarios SMN
+		Data..................:	10/05/2024
+		Ex....................:	DBCC FREEPROCCACHE
+								DBCC DROPCLEANBUFFERS
+
+								DECLARE @DataInicio DATETIME = GETDATE()
+
+								SELECT [dbo].[FNC_CalcularValorParcela](150000.00, 1, 60, 2)
+
+								SELECT DATEDIFF(MILLISECOND, @DataInicio, GETDATE()) AS TempoExecucao
+	*/
+	BEGIN
+		--Declarar vari�vel
+		DECLARE @ValorTotal DECIMAL(10,2)
+
+		--Calcular valor
+		SET @ValorTotal = @ValorVenda / @TotalParcela + (CASE	WHEN @Financiado = 1 THEN(@ValorVenda * (SELECT MAX(vi.Aliquota)
+																											FROM [dbo].[ValorIndice] vi WITH(NOLOCK)
+																											WHERE vi.IdIndice = @IdIndice
+																										 )
+																						  )
+																ELSE 0
+														  END
+														)
+		RETURN @ValorTotal
+	END
+GO
---
+++ DB_ConstrutoraLMNC/Procedures/Apartamento.sql
@@ -0,0 +1,52 @@
+CREATE OR ALTER PROC [dbo].[SP_RelatorioAptoVendidos]
+	@IdVenda INT = NULL
+	AS
+	/*
+		Documentacao
+		Arquivo Fonte.....: Apartamento.sql
+		Objetivo..........: Listar todos os apartamentos com as informacoes de parcelas pagas, vencidas e vincendas
+		Autor.............: Grupo de estagiarios SMN
+		Data..............: 10/05/2024
+		Ex................:	DBCC FREEPROCCACHE
+							DBCC DROPCLEANBUFFERS
+							DBCC FREESYSTEMCACHE('ALL')
+
+							DECLARE @DATA_INI DATETIME = GETDATE();
+
+							EXEC [dbo].[SP_RelatorioAptoVendidos]
+
+							SELECT DATEDIFF(MILLISECOND, @DATA_INI, GETDATE()) AS TempoExecucao;
+	*/
+	BEGIN
+		-- Declarando variavel da data atual
+		DECLARE @DataAtual DATE = GETDATE();
+		
+		-- Recuperando dados da subquery e calculando tambem a quantidade de parcelas restantes
+		SELECT	v.Id AS IdVenda,
+				v.TotalParcela AS TotalParcela,
+				x.QuantidadeParcelaPaga AS QuantidadeParcelaPaga,
+				x.QuantidadeParcelaVencida AS QuantidadeParcelaVencida,
+				(v.TotalParcela - x.QuantidadeParcelaPaga - x.QuantidadeParcelaVencida) AS ParcelasRestantes
+			FROM	(
+						-- Recuperando dados do id da venda, contagem de parcelas pagas e contagem de parcelas vencidas
+						SELECT	p.IdVenda AS IdVenda,
+								(
+									SELECT	COUNT(Id)
+										FROM [dbo].[Parcela] WITH(NOLOCK)
+										WHERE IdLancamento IS NOT NULL
+											AND IdVenda = p.IdVenda
+								) AS QuantidadeParcelaPaga,
+								(
+									SELECT	COUNT(Id)
+										FROM [dbo].[Parcela] WITH(NOLOCK)
+										WHERE IdLancamento IS NULL
+											AND DataVencimento < @DataAtual
+											AND IdVenda = p.IdVenda
+								) AS QuantidadeParcelaVencida
+							FROM [dbo].[Parcela] p WITH(NOLOCK)
+							WHERE IdVenda = ISNULL(@IdVenda, p.IdVenda)
+					) AS x
+				INNER JOIN [dbo].[Venda] v WITH(NOLOCK)
+					ON x.IdVenda = v.Id;
+	END
+GO
