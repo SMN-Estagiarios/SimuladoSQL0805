fdd9255390fe50417b3a9a8cb611261eec780ff9
Gustavo Targino <gustavo.targinocc@hotmail.com>
2024-05-10 23:52:22 -0300

    FEAT: Relatório de Extrato de Contas

--- Construtora/PROCEDURES/conta.sql
+++ Construtora/PROCEDURES/conta.sql
@@ -86,3 +86,76 @@ AS
 		DROP TABLE #TabelaData
 END		
 	
+GO
+CREATE OR ALTER PROCEDURE [dbo].[SP_RelatorioExtratoContas]
+	@Ano INT,
+	@Mes INT
+AS
+	/*
+		Documentação
+		Arquivo Fonte.....: Conta.sql
+		Objetivo..........: Listar o extrato das contas com base no último mês
+		Autor.............: Gustavo Targino
+		Data..............: 10/05/2024
+		Ex................: BEGIN TRAN
+								DBCC DROPCLEANBUFFERS;
+								DBCC FREEPROCCACHE;
+
+								DECLARE @RET INT, 
+								@DataInicio DATETIME = GETDATE()
+
+								EXEC [dbo].[SP_RelatorioExtratoContas] 2024, 04
+								
+								SELECT DATEDIFF(MILLISECOND, @DataInicio, GETDATE()) Tempo
+
+							ROLLBACK TRAN
+	*/
+	 BEGIN
+		DECLARE @DataInicio DATE,
+				@DataFim DATE
+
+		-- Usando o dia 1 do mês e ano solicitado até o último dia desse mês e desse ano
+		SET @DataInicio = DATEFROMPARTS(@Ano, @Mes, 01)
+		SET @DataFim = EOMONTH(@DataInicio) 
+
+		-- Tabela de calendário
+		CREATE TABLE #TabelaData(
+									DataSaldo DATE
+								)
+				
+		--Populando tabela de calendário
+		WHILE @DataInicio <= @DataFim
+			BEGIN
+				INSERT INTO #TabelaData(DataSaldo) 
+					VALUES	(@DataInicio)
+				SET @DataInicio = DATEADD(DAY, 1, @DataInicio)
+			END;
+
+		WITH CalculoCreditoDebito AS (
+										SELECT  ISNULL(SUM(CASE WHEN TipoOperacao = 'C' THEN Valor ELSE 0 END), 0) AS Valor_Credito,
+												ISNULL(SUM(CASE WHEN TipoOperacao = 'D' THEN Valor ELSE 0 END), 0) AS Valor_Debito,
+												td.DataSaldo,
+												C.Id AS Id_Conta
+											FROM #TabelaData td
+												CROSS JOIN [dbo].[Conta] C WITH(NOLOCK)
+												LEFT JOIN [dbo].[Lancamento] la WITH(NOLOCK)
+													ON DATEDIFF(DAY, td.DataSaldo, la.DataLancamento) = 0 
+													AND C.Id = la.IdConta
+											WHERE  td.DataSaldo >= C.DataAbertura
+											GROUP BY td.DataSaldo, c.Id
+								   	)	
+											SELECT  Id_Conta,
+													ISNULL(LAG(Saldo_Final, 1, 0) OVER (PARTITION BY Id_Conta ORDER BY DataSaldo), 0) AS Saldo_Inicial,
+													Credito,
+													Debito,
+													Saldo_Final,
+													DataSaldo
+												FROM (
+														SELECT  Valor_Credito Credito,
+																Valor_Debito Debito,
+																DataSaldo DataSaldo,
+																Id_Conta Id_Conta,
+																SUM(Valor_Credito - Valor_Debito) OVER (PARTITION BY Id_Conta ORDER BY DataSaldo) AS Saldo_Final
+															FROM CalculoCreditoDebito
+									)x				
+END	
